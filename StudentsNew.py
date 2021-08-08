3e20a05688ea82023e89757e71af4cef3690cbee
Shubh1am <88609379+Shubh1am@users.noreply.github.com>
2021-08-08 14:17:53 +0530

    Update StudentsNew.py

--- StudentsNew.py
+++ StudentsNew.py
@@ -17,13 +17,15 @@ plt.ylabel('The Percentage Score')
 plt.show()
 
 
-
+#divide the data into attributes and labels 
 X = data.iloc[:, :-1].values    
 y = data.iloc[:, 1].values  
 
+#split data into train and test dataset
 from sklearn.model_selection import train_test_split    
 X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.2, random_state=0)   
 
+#train data
 from sklearn.linear_model import LinearRegression    
 regressor = LinearRegression()    
 regressor.fit(X_train, y_train)   
@@ -33,10 +35,11 @@ plt.scatter(X, y)
 plt.plot(X, line);  
 plt.show()
 
+#predict
 print(X_test)   
 y_pred = regressor.predict(X_test)  
 
-
+#Actual Vs Predict
 df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})    
 df
 
